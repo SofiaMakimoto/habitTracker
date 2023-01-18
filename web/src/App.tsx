055434552d5280daaa4f5e99a4197178c484b53a
Sofia Makimoto <sofiamakimoto@gmail.com>
2023-01-17 22:56:14 -0300

    add basic server and web aplications

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import { Habit } from './components/Habit'
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <div>
+        <Habit />
+        <Habit />
+        <Habit />
+        <Habit />
+        <Habit />
+
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,7 @@
+export function Habit() {
+    return (
+        <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex justify-center items-center">
+            Teste
+        </div>
+    )
+}
